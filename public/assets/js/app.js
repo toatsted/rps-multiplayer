eb50de0400ab8f928bf5f9bbd638453a6e2abca8
Sam <samueljacksonhicks@gmail.com>
2018-03-11 00:02:05 -0600

    chat and colors working. chat handler cheaned out

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,33 +1,23 @@
 let database = firebase.database();
 
 
-$("#killSwitch").on("click", function(){
-	database.ref("queue/").remove();
-	database.ref("game/").remove();
-	database.ref("chat/").remove();
+$("#killSwitch").on("click", function() {
+	database.remove();
 })
 
 
 $(() => {
 	let title = $("#title");
+	let Player = { name: "" }
+	let Opponent = { name: "" }
 
-	let pTag;
-
-	let Player = {
-		name: ""
-	}
-
-	let Opponent = {
-		name: ""
-	}
-
-	function startGame(){
+	function startGame() {
 		database.ref("chat/").remove();
 		$("#message-box").removeClass("hide");
 
 		$(".playArea").empty();
 		$("#title").text("Supreme");
-		
+
 		$("#playerName").text(Player.name);
 		$("#opponentName").text(Opponent.name);
 	}
@@ -36,14 +26,14 @@ $(() => {
 		database.ref(key).on("value", function(snapshot) {
 			let data = snapshot.val();
 
-			
-			if(!data){
+
+			if (!data) {
 				data = [];
 			}
 
-			switch(key){
+			switch (key) {
 				case "queue/":
-					if(data.length >= 2){
+					if (data.length >= 2) {
 						// pair them in a game
 						database.ref("game/").set(data);
 						database.ref("queue/").remove();
@@ -59,39 +49,35 @@ $(() => {
 
 						data.push(input);
 						database.ref("queue/").set(data);
-						console.log(data);
 					})
-				break;
+					break;
 				case "game/":
 					data.forEach((value) => {
-						if(!(value === Player.name)){
+						if (!(value === Player.name)) {
 							Opponent.name = value;
 						}
 					})
-				break;
+					break;
 				case "chat/":
 					$("#messages").empty();
 					data.forEach((value) => {
-						$("#messages").prepend(value);
-						console.log(value);
+						$("#messages").prepend(
+							$("<p>").text(": " + value.message).prepend(
+								$("<span>").text(value.name).addClass(
+									(Player.name === value.name) ? "redText" : "")));
 					})
 
-					$("#message-form").off("submit").on("submit", function(e){
+					$("#message-form").off("submit").on("submit", function(e) {
 						e.preventDefault();
 
 						let message = $("#message-input").val().trim();
 						$("#message-input").val("");
 
-						let playerName = $("<span>").text(Player.name);
-						let pTag = $("<p>").text(": " + message).prepend(playerName);
-						// pTag = "<p><span>" + Player.name + ": </span>" + message + "</p>";
-						
-						$("#messages").prepend(pTag);
-						data.push(pTag);
+						data.push({ message: message, name: Player.name });
 						database.ref("chat/").set(data);
 					})
 
-				break;
+					break;
 			}
 
 		})
